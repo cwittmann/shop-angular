e64da95bfa39290d04aaa38ce921dc4635f3abfd
cwittmann <christofwittmann@web.de>
2020-06-18 15:38:00 +0200

    feat: spinner / login ui

--- src/app/component/common/title/title.component.ts
+++ src/app/component/common/title/title.component.ts
@@ -1,9 +1,26 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {
+  trigger,
+  state,
+  transition,
+  animate,
+  style,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-title',
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.scss'],
+  animations: [
+    trigger('scroll', [
+      state('in', style({ opacity: 1.0, transform: 'translate(0,0)' })),
+      transition(':enter', [
+        style({ opacity: 0.5, transform: 'translate(0,-100px)' }),
+        animate('400ms cubic-bezier(0.680, -0.550, 0.265, 1.550)'),
+      ]),
+      transition(':leave', animate(600, style({ opacity: 0 }))),
+    ]),
+  ],
 })
 export class TitleComponent implements OnInit {
   @Input()
